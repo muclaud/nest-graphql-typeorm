3d392a3512070053f2593a79b7efc3ea6c24c8ad
muclaud <ttunka08@gmail.com>
2021-12-17 17:54:04 +0200

    add redis and pubsub

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { Upload } from './common/scalars/upload.scalar';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { MailModule } from './email/mailer.module';
+import { PubSubModule } from './pubSub/pubSub.module';
 import { FileUploadModule } from './file-upload/file-upload.module';
 import { ChatModule } from './chat/chat.module';
 import * as Joi from 'joi';
@@ -27,6 +28,8 @@ import * as Joi from 'joi';
         EMAIL_SERVER_USER: Joi.string().required(),
         EMAIL_SERVER_PASSWORD: Joi.string().required(),
         EMAIL_FROM: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
       }),
     }),
     GraphQLModule.forRoot({
@@ -46,6 +49,7 @@ import * as Joi from 'joi';
     PostsModule,
     FileUploadModule,
     ChatModule,
+    PubSubModule,
   ],
   controllers: [],
   providers: [Upload],
